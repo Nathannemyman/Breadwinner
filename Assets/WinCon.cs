6cd9cfc2cd023dd6d7ac6613ba2fabeae92ac8ac
PoyoMain <CLC9512@GMAIL.COM>
2025-03-30 01:23:22 -0400

    Rough Pause Menu w art implemented

--- Assets/WinCon.cs
+++ Assets/WinCon.cs
@@ -6,17 +6,15 @@ public class WinCon : MonoBehaviour
 {
     public UnityEvent onGameWin = default;
 
-    private bool GameWon = false;
     public PogoStickMovement player;
 
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent(out PogoStickMovement player))
+        if (collision.TryGetComponent(out PogoStickMovement _))
         {
             if (GameManager.Instance.HasBread)
             {
-                GameWon = true;
                 onGameWin?.Invoke();
             }
         }
