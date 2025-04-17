6de68a33fab23c3bf6b1fcb68866e72bfe5c587a
Nathannemyman <professionalnathan.business@gmail.com>
2025-04-16 23:58:52 -0400

    Edited Transfer Folder
    Added fonts, and collectible elements in inspector.

--- Assets/Transfer/DeathShop.cs
+++ Assets/Transfer/DeathShop.cs
@@ -26,6 +26,8 @@ public class DeathShop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        try
+        {
             for (int i = 0; i < Items.Count + 2; i++)
             {
                 int rnd = Random.Range(0, Items.Count);
@@ -35,7 +37,10 @@ public class DeathShop : MonoBehaviour
 
                 ItemsCost[i].text = ItemsOut[i].cost.ToString();
             }
-    }
+        }
+        catch { }
+        }
+
 
     // Update is called once per frame
     void Update()
