64da19e0127ea5b1240f5f631bb0537fa240304a
PoyoMain <CLC9512@GMAIL.COM>
2025-04-24 23:50:58 -0400

    Update DeathShop.cs

--- Assets/DeathTransfer/DeathShop.cs
+++ Assets/DeathTransfer/DeathShop.cs
@@ -39,7 +39,7 @@ public class DeathShop : MonoBehaviour
 
         }
 
-        List<CollectableSO> availableCollectables = collectables.Where(x => GameData.Instance.HasItem(x.Type)).ToList();
+        List<CollectableSO> availableCollectables = collectables.Where(x => !GameData.Instance.HasItem(x.Type)).ToList();
         if (availableCollectables.Count <= 0) return;
 
         for (int i = 0; i < buttons.Length; i++)
