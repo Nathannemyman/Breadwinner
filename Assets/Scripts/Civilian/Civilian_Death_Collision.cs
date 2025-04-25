1c9689e6e526673ed4b9156d969c7be0bc5019c5
Myth-Roses <122834373+Myth-Roses@users.noreply.github.com>
2025-04-24 21:00:18 -0400

    Update Civilian_Death_Collision.cs

--- Assets/Scripts/Civilian/Civilian_Death_Collision.cs
+++ Assets/Scripts/Civilian/Civilian_Death_Collision.cs
@@ -130,6 +130,7 @@ public class DeathCollision : MonoBehaviour
                 styleBonus = 3;
             }
             pogoStickMovement.frontFlipped = false;
+            //
             //StartCoroutine(PlayKillSFX());
             //AudioSource.PlayClipAtPoint(killCivilianSFX, transform.position);
         }
